1ac0c7a5d3cb41d84db8bbde10e99b09d675fab0
Delgado-Jose <jdelgado@frigoyi.com.uy>
2024-09-06 19:01:21 -0300

    [C]: Entrega Final ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navBar/navBar.jsx';
+import NavBar from './components/navBar/NavBar.jsx';
 import ItemListContainer from './components/itemListContainer/ItemListContainer.jsx';
 import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer.jsx';
 import Cart from './components/cart/cart.jsx';
